cc04a51a442d1af922b30501e85e1abd062838c3
kassak <kass.soft@gmail.com>
2015-01-18 19:49:14 +0400

    android compilation

--- sources/android/project_create.bat
+++
@@ -1,6 +0,0 @@
-android create project ^
- --target 1 ^
- --name netput_source_android ^
- --path .\ ^
- --activity NSAndroidActivity ^
- --package org.netput.sources.android
--- sources/android/project_create.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-android create project \
- --target 1 \
- --name netput_source_android \
- --path ./ \
- --activity NSAndroidActivity \
- --package org.netput.sources.android
